ec6106706125d2bbb681fbc936b31df4e4f23f7b
Szogun <szogunstyle@gmail.com>
2024-11-09 12:06:29 +0100

    Zmiany frontendu komponentow Fan i Plug

--- Raspberry/tauri/test/src/components/PlugStripComponent.jsx
+++ Raspberry/tauri/test/src/components/PlugStripComponent.jsx
@@ -1,7 +1,24 @@
 import styles from "../styles/components/PlugStripComponent.module.scss";
+import { GiPlainCircle } from "react-icons/gi";
 
 const PlugStripComponent = () => {
-    return <p className={styles.doUsuniecia}>Listwa</p>;
+    return (
+        <div className={styles.PlugContent}>
+            <div className={styles.PlugChart}>
+                
+            </div>
+            <div className={styles.Plug}>
+                <div className={styles.CircleOn}>
+                    <GiPlainCircle className={styles.Dot} />
+                    <GiPlainCircle className={styles.Dot} />
+                </div>
+                <div className={styles.CircleOff}>
+                    <GiPlainCircle className={styles.Dot} />
+                    <GiPlainCircle className={styles.Dot} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default PlugStripComponent;
