195cb8e39497317df8fdd2f57261fae754607f35
giro39 <jan.wysmolinski@student.put.poznan.pl>
2024-11-13 15:28:54 +0100

    Add basic line chart with mock data

--- Raspberry/tauri/test/src/App.jsx
+++ Raspberry/tauri/test/src/App.jsx
@@ -2,35 +2,39 @@
 
 import React, { useState, useEffect } from "react";
 import styles from "./styles/App.module.scss";
-import ToggleButton from "./button";  // Zaimportowanie komponentu ToggleButton
+import ToggleButton from "./button"; // Zaimportowanie komponentu ToggleButton
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import FanComponent from "./components/FanComponent";
 import PlugStripComponent from "./components/PlugStripComponent";
 import LedStripComponent from "./components/LedStripComponent";
+import Chart from "./components/Chart";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-const client = new W3CWebSocket('ws://localhost:8765');
-const device_id = "Front"
+const client = new W3CWebSocket("ws://localhost:8765");
+const device_id = "Front";
 
 const App = () => {
     const [selectedModule, setSelectedModule] = useState("home");
-    const [buttonState, setButtonState] = useState(false); 
+    const [buttonState, setButtonState] = useState(false);
     const [temperature, setTemperature] = useState(0);
 
+    const mockTemperatureData = Array.from({ length: 100 }, () =>
+        Math.floor(Math.random() * 30 + 10)
+    );
+
     const handleButtonClick = (newState) => {
-        setButtonState(newState);  
+        setButtonState(newState);
         console.log("Stan przycisku:", newState ? "ON" : "OFF");
         const data = {
             sender_id: device_id,
             data: newState ? "ON" : "OFF",
-            "target_id": "Temp"
+            target_id: "Temp",
         };
         client.send(JSON.stringify(data));
     };
 
     useEffect(() => {
-
         client.onopen = () => {
             console.log(`${device_id} connected to the server.`);
             client.send(device_id);
@@ -38,7 +42,9 @@ const App = () => {
 
         client.onmessage = (message) => {
             const data = JSON.parse(message.data);
-            console.log(`${device_id} received data from server: ${data.command.data}°C`);
+            console.log(
+                `${device_id} received data from server: ${data.command.data}°C`
+            );
             if (data.command.sender_id === "Temp") {
                 setTemperature(data.command.data);
             }
@@ -66,8 +72,12 @@ const App = () => {
 
     return (
         <main>
-            <Navbar selectedModule={selectedModule} onModuleSelect={setSelectedModule} />
+            <Navbar
+                selectedModule={selectedModule}
+                onModuleSelect={setSelectedModule}
+            />
             {renderModuleComponent()}
+            <Chart data={mockTemperatureData} type="temperature" />
             <ToggleButton onClick={handleButtonClick} />
             <div className={styles.temperature}>
                 <h2>Temperature: {temperature}°C</h2>
