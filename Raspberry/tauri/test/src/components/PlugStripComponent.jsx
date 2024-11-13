740f47e2c55e8a32e81dda2d06d4443e4a95ff26
Szogunisko <janekzajac2003@gmail.com>
2024-11-13 17:10:18 +0100

    Poprawki guzika i mozliwosc wlaczenia/wylaczenia listwy

--- Raspberry/tauri/test/src/components/PlugStripComponent.jsx
+++ Raspberry/tauri/test/src/components/PlugStripComponent.jsx
@@ -1,18 +1,38 @@
+import { useState } from "react";
 import styles from "../styles/components/PlugStripComponent.module.scss";
 import { GiPlainCircle } from "react-icons/gi";
 
 const PlugStripComponent = () => {
+
+    const [isOn1, setIsOn1] = useState(false);
+    const [isOn2, setIsOn2] = useState(false);
+
+    const toggleCircle1 = () => {
+        setIsOn1(prevIsOn => !prevIsOn);
+    };
+
+    const toggleCircle2 = () => {
+        setIsOn2(prevIsOn => !prevIsOn);
+    };
+
     return (
         <div className={styles.PlugContent}>
             <div className={styles.PlugChart}>
-                
             </div>
+
             <div className={styles.Plug}>
-                <div className={styles.CircleOn}>
+                <div
+                    className={isOn1 ? styles.CircleOn : styles.CircleOff}
+                    onClick={toggleCircle1}
+                >
                     <GiPlainCircle className={styles.Dot} />
                     <GiPlainCircle className={styles.Dot} />
                 </div>
-                <div className={styles.CircleOff}>
+
+                <div
+                    className={isOn2 ? styles.CircleOn : styles.CircleOff}
+                    onClick={toggleCircle2}
+                >
                     <GiPlainCircle className={styles.Dot} />
                     <GiPlainCircle className={styles.Dot} />
                 </div>
