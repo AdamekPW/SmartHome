087225217578cbeff2eaf54f533cd59b222d94e8
filip <filip.witkowicz1@gmail.com>
2024-11-26 15:26:05 +0100

    add global variables

--- Raspberry/Flask/server.py
+++ Raspberry/Flask/server.py
@@ -99,6 +99,7 @@ async def send_command_to_device(sender_id, command):
         print(f"Device {sender_id} not connected.")
 
 async def process_device_data(data):
+    global ESP1_data, ESP2_data, ESP3_data
     # Weryfikacja, że dane są w prawidłowym formacie
     if "data" not in data:
         print("Invalid data format received.")
