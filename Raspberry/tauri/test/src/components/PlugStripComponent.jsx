ba44058fb0d23b96479077a87ab3d2667814541d
giro39 <jan.wysmolinski@student.put.poznan.pl>
2024-11-28 12:17:37 +0100

    Refactor PlugStripComponent code

--- Raspberry/tauri/test/src/components/PlugStripComponent.jsx
+++ Raspberry/tauri/test/src/components/PlugStripComponent.jsx
@@ -13,9 +13,7 @@ const PlugStripComponent = ({
     plugButtonInfo,
     dbPower,
 }) => {
-    const [isOn1, setIsOn1] = useState(false);
-    const [isOn2, setIsOn2] = useState(false);
-    const [isOn3, setIsOn3] = useState(false);
+    const [buttonStates, setButtonStates] = useState([false, false, false]);
 
     const { plugLivePowerData, setPlugLivePowerData } = useContext(
         PlugStripLivePowerContext
@@ -32,62 +30,21 @@ const PlugStripComponent = ({
     }, [plugPower]);
 
     useEffect(() => {
-        if (plugButtonInfo[0] === "1") setIsOn1(true);
-        else setIsOn1(false);
-        if (plugButtonInfo[1] === "1") setIsOn2(true);
-        else setIsOn2(false);
-        if (plugButtonInfo[2] === "1") setIsOn3(true);
-        else setIsOn3(false);
+        const newStatesBool = plugButtonInfo.map((info) => info === "1");
+        setButtonStates(newStatesBool);
     }, [plugButtonInfo]);
 
-    const toggleCircle1 = () => {
-        const newState = !isOn1;
-        setIsOn1(newState);
-        console.log("Stan przycisku 1:", newState ? "1" : "0");
-        const data = {
-            sender_id: device_id,
-            data:
-                (newState ? "1" : "0") +
-                "|" +
-                (isOn2 ? "1" : "0") +
-                "|" +
-                (isOn3 ? "1" : "0"),
-            target_id: "ESP2",
-        };
-        client.send(JSON.stringify(data));
-    };
+    const toggleCircle = (index) => {
+        const newStates = [...buttonStates];
+        newStates[index] = !newStates[index];
+        setButtonStates(newStates);
 
-    const toggleCircle2 = () => {
-        const newState = !isOn2;
-        setIsOn2(newState);
-        console.log("Stan przycisku 2:", newState ? "1" : "0");
         const data = {
             sender_id: device_id,
-            data:
-                (isOn1 ? "1" : "0") +
-                "|" +
-                (newState ? "1" : "0") +
-                "|" +
-                (isOn3 ? "1" : "0"),
+            data: newStates.map((state) => (state ? "1" : "0")).join("|"),
             target_id: "ESP2",
         };
-        client.send(JSON.stringify(data));
-    };
 
-    const toggleCircle3 = () => {
-        const newState = !isOn3;
-        setIsOn3(newState);
-        console.log("Stan przycisku 2:", newState ? "1" : "0");
-        const data = {
-            sender_id: device_id,
-            data:
-                (isOn1 ? "1" : "0") +
-                "|" +
-                (isOn2 ? "1" : "0") +
-                "|" +
-                (newState ? "1" : "0"),
-            target_id: "ESP2",
-        };
         client.send(JSON.stringify(data));
     };
 
@@ -100,29 +57,18 @@ const PlugStripComponent = ({
             </div>
             <div className={styles.ButtonsWithSample}>
                 <div className={styles.Plug}>
-                    <div
-                        className={isOn1 ? styles.CircleOn : styles.CircleOff}
-                        onClick={toggleCircle1}
-                    >
-                        <GiPlainCircle className={styles.Dot} />
-                        <GiPlainCircle className={styles.Dot} />
-                    </div>
-
-                    <div
-                        className={isOn2 ? styles.CircleOn : styles.CircleOff}
-                        onClick={toggleCircle2}
-                    >
-                        <GiPlainCircle className={styles.Dot} />
-                        <GiPlainCircle className={styles.Dot} />
-                    </div>
-
-                    <div
-                        className={isOn3 ? styles.CircleOn : styles.CircleOff}
-                        onClick={toggleCircle3}
-                    >
-                        <GiPlainCircle className={styles.Dot} />
-                        <GiPlainCircle className={styles.Dot} />
-                    </div>
+                    {buttonStates.map((isOn, index) => (
+                        <div
+                            key={index}
+                            className={
+                                isOn ? styles.CircleOn : styles.CircleOff
+                            }
+                            onClick={() => toggleCircle(index)}
+                        >
+                            <GiPlainCircle className={styles.Dot} />
+                            <GiPlainCircle className={styles.Dot} />
+                        </div>
+                    ))}
                 </div>
                 <div className={styles.CurrentPowerSample}>{plugPower}W</div>
             </div>
